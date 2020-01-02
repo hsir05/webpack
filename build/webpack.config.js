c439ae9dfcee84125a2945c96a951c37a21d471d
hdq <haqsir@163.com>
2020-01-02 10:16:02 +0800

    功能丰富优化

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Webpack = require('webpack')
-// let indexScss = new ExtractTextWebpackPlugin('index.scss');
-// let indexCss = new ExtractTextWebpackPlugin('index.css');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const Webpack = require('webpack');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     mode: 'development', // 开发模式
     entry: {
-        main: path.resolve(__dirname, '../src/main.js'),
-        header: path.resolve(__dirname, '../src/header.js')
+        main: ["@babel/polyfill", path.resolve(__dirname, '../src/main.js')],
+        header: ["@babel/polyfill", path.resolve(__dirname, '../src/header.js')]
     },
     output: {
         filename: '[name].[hash:8].js',      // 打包后的文件名称
@@ -18,6 +18,16 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/
+            },
             {
                 test: /\.css$/,
                 use: ['css-loader', {
@@ -35,24 +45,58 @@ module.exports = {
                         plugins: [require('autoprefixer')]
                     }
                 }, 'scss-loader']
-            }
-
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'css-loader'] // 从右向左解析原则
-            // },
-            // {
-            //     test: /\.scss$/,
-            //     use: [
-            //             MiniCssExtractPlugin.loader,
-            //             'style-loader', 'css-loader', {
-            //             loader: 'postcss-loader',
-            //             options: {
-            //                 plugins: [require('autoprefixer')]
-            //             }
-            //         }, 'scss-loader'
-            //     ] // 从右向左解析原则
-            // }
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i, //图片文件
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'img/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'media/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'fonts/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
         ]
     },
     plugins: [
@@ -67,11 +111,23 @@ module.exports = {
             chunks: ['header'] // 与入口文件对应的模块名
         }),
         new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(['dist/*.js'], {
+        //     verbose: true,
+        //     dry: false
+        // }),
         new MiniCssExtractPlugin({
             filename: "[name].[hash].css",
             chunkFilename: "[id].css",
         }),
-        new Webpack.HotModuleReplacementPlugin()
+        new Webpack.HotModuleReplacementPlugin(),
+        // 分离 CSS 代码
+        new ExtractTextPlugin("css/[name].[contenthash].css"),
+        // 压缩提取出的 CSS，并解决ExtractTextPlugin分离出的 JS 重复问题
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+                safe: true
+            }
+        }),
     ],
     devServer: {
         port: 3000,
